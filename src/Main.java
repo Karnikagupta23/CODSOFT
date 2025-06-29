59de001e7ff6b42ee06322125fd17b3768261360
Karnikagupta23 <guptakarnika962@gmail.com>
2025-06-29 23:26:58 +0530

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount("Karnika Gupta", "ITMU2025", 10000.00);
-        ATM atmMachine = new ATM(myAccount);
-        atmMachine.start();
-    }
-}
